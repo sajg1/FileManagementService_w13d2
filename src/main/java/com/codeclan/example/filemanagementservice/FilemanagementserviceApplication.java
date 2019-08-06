abd72f8db62f5fb07b936e8022c75f04bca63be3
Steve Gossip <stephengossip@gmail.com>
2019-08-06 16:11:17 +0100

    filemanagement project created

---
+++ src/main/java/com/codeclan/example/filemanagementservice/FilemanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilemanagementserviceApplication.class, args);
+	}
+
+}
